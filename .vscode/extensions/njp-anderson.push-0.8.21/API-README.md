a8de512a9e34e55a0d416c3bae4d59a0d3a703c5
Qbit <Qbitcurrency@aol.com>
2021-01-23 18:59:18 +0000

    commit

--- .vscode/extensions/njp-anderson.push-0.8.21/API-README.md
+++
@@ -1,1 +0,0 @@
-This is the API documentation for Push, a Visual Code Extension. Please note that full documentation and examples of use are available on [GitHub](https://github.com/njpanderson/push)
--- .vscode/extensions/njp-anderson.push-0.8.21/webpack.analyse.config.js
+++
@@ -1,8 +0,0 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const config = require('./webpack.config.js');
-
-config.plugins.push(
-	new BundleAnalyzerPlugin()
-);
-
-module.exports = config;
